886e825ccc57c1db71a5c6afba3d4d02301dc0c8
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2022-06-24 16:14:58 +0300

    ref: 4 modules

--- index.ts
+++ index.ts
@@ -1,33 +1,40 @@
-import { WebSocketServer, CloseEvent } from "ws";
+import { WebSocketServer, CloseEvent, createWebSocketStream } from "ws";
 import { httpServer } from "./src/http_server/index";
 import { performCommand } from "./src/performCommand";
+import { Messages } from "./src/constatnts";
+import "dotenv/config";
 
-const HTTP_PORT = 3000;
+const HTTP_PORT = process.env["HTTP_PORT"];
+const WS_PORT = process.env["WS_PORT"] ? +process.env["WS_PORT"] : 0;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 const wss = new WebSocketServer({
-  port: 8080,
+  port: WS_PORT,
 });
 
-wss.on("connection", (ws) => {
-  //const duplex = createWebSocketStream(ws);
-  //duplex.write("Web Socket Connection Is Succesful!");
-  ws.send("Web Socket Connection Is Succesful!");
+//https://github.com/websockets/ws/issues/1640
 
-  ws.on("message", (data) => {
+wss.on("connection", async (ws) => {
+  const messageStream = createWebSocketStream(ws, { encoding: "utf8" });
+  //duplex.write("Web Socket Connection Is Succesful!");
+  //ws.send(Messages.WS_CONNECTION_SUCCES);
+
+  for await (let message of messageStream) {
+    console.log("received: %s", message);
+    performCommand(message, ws);
+  }
+  /* ws.on("message", (data) => {
     console.log("received: %s", data);
     performCommand(data, ws);
-  });
+  }); */
 });
 
 wss.on("close", (event: CloseEvent) => {
   if (event.wasClean) {
-    console.log(
-      `The connection is closed cleanly,  code=${event.code} reason=${event.reason}`
-    );
+    console.log(Messages.WS_CONNECTION_COSE_CLEAR, event.code, event.reason);
   } else {
-    console.log("The connection is interrupted");
+    console.log(Messages.WS_CONNECTION_INTERAPT);
   }
 });
--- src/performCommand.ts
+++ src/performCommand.ts
@@ -2,55 +2,64 @@ import { RawData, WebSocket } from "ws";
 import robot from "robotjs";
 import Jimp from "jimp";
 import { Commands } from "./command";
-//import Jimp from "jimp/*";
-
-robot.setMouseDelay(1);
-const offsetMouseDraw = 0.01;
-const END_COMMAND = "\0";
+import {
+  drawCircle,
+  drawRectangle,
+  performWithLeftMouseButton,
+  screenCaptureToFile,
+} from "./drawRobotJs";
 
 export const performCommand = async (data: RawData, ws: WebSocket) => {
   try {
-    //const duplex = createWebSocketStream(ws)
+    //https://github.com/websockets/ws
+    /* const messageStream = createWebSocketStream(ws, {
+      encoding: "utf8",
+      objectMode: true,
+      writableObjectMode: true,
+    }); */
+
+    //messageStream.pipe(ws.);
+
     const { command, firstParam, secondParam } = parseParams(data);
     switch (command) {
       case Commands.DRAW_CIRCLE:
         //await getUsers(req, res);
         const radius = firstParam!;
-        ws.send(Commands.DRAW_CIRCLE + END_COMMAND);
+        ws.send(Commands.DRAW_CIRCLE);
         performWithLeftMouseButton(drawCircle, radius);
         break;
 
       case Commands.DRAW_RECTANGLE:
         const widthRect = firstParam!;
         const heightRect = secondParam!;
-        ws.send(Commands.DRAW_RECTANGLE + END_COMMAND);
+        ws.send(Commands.DRAW_RECTANGLE);
         performWithLeftMouseButton(drawRectangle, widthRect, heightRect);
         break;
 
       case Commands.DRAW_SQUARE:
         const widthSquare = firstParam!;
-        ws.send(Commands.DRAW_SQUARE + END_COMMAND);
+        ws.send(Commands.DRAW_SQUARE);
         performWithLeftMouseButton(drawRectangle, widthSquare, widthSquare);
         break;
 
       case Commands.MOUSE_POSITION:
         const mousePosition = robot.getMousePos();
-        const resMousePosition = `${Commands.MOUSE_POSITION} {${mousePosition.x}},{${mousePosition.y}}`;
-        ws.send(resMousePosition + END_COMMAND);
+        const resMousePosition = `${Commands.MOUSE_POSITION} ${mousePosition.x},${mousePosition.y}`;
+        ws.send(resMousePosition);
         break;
 
       case Commands.MOUSE_UP:
         const offsetUp = firstParam!;
         const mousePositionUp = robot.getMousePos();
         robot.moveMouse(mousePositionUp.x, mousePositionUp.y - offsetUp);
-        ws.send(Commands.MOUSE_UP + END_COMMAND);
+        ws.send(Commands.MOUSE_UP);
         break;
 
       case Commands.MOUSE_DOWN:
         const offsetDown = firstParam!;
         const mousePositionDown = robot.getMousePos();
         robot.moveMouse(mousePositionDown.x, mousePositionDown.y + offsetDown);
-        ws.send(Commands.MOUSE_DOWN + END_COMMAND);
+        ws.send(Commands.MOUSE_DOWN);
         break;
 
       case Commands.MOUSE_RIGHT:
@@ -60,84 +69,36 @@ export const performCommand = async (data: RawData, ws: WebSocket) => {
           mousePositionRight.x + offsetRight,
           mousePositionRight.y
         );
-        ws.send(Commands.MOUSE_RIGHT + END_COMMAND);
+        ws.send(Commands.MOUSE_RIGHT);
         break;
 
       case Commands.MOUSE_LEFT:
         const offsetLeft = firstParam!;
         const mousePositionLef = robot.getMousePos();
         robot.moveMouse(mousePositionLef.x - offsetLeft, mousePositionLef.y);
-        ws.send(Commands.MOUSE_LEFT + END_COMMAND);
+        ws.send(Commands.MOUSE_LEFT);
         break;
 
       case Commands.PRINT_SCREEN:
         const size = 200;
         const mousePositionPrnScrn = robot.getMousePos();
-
         const img = robot.screen.capture(
           mousePositionPrnScrn.x,
           mousePositionPrnScrn.y,
           size,
           size
         );
-
-        //const imageBuffer = await Jimp.read(img.image);
-
-        /* const imageBuffer = await Jimp.read(img.image, function (err, image) {
-          if (err) {
-            console.log(err.message);
-          } else {
-            image.write("new-image.png");
-          }
-        }); */
-
-        //https://stackoverflow.com/questions/41941151/capture-and-save-image-with-robotjs
-
-        /* const width = size;
-        const height = size;
-        const imageBufferJimp = new Jimp(
-          { data: img.image, width, height },
-          //@ts-ignore
-          (err, image) => {
-            image.write("temp.png");
-          }
-        ); */
-
         await screenCaptureToFile(img, "temp.png");
-
         const imageBufferBase64 = (await (await Jimp.read("temp.png"))
           .getBase64Async(Jimp.MIME_PNG)
           .catch((err) => {
             console.log(err.message);
           }))!;
-
-        //const temp = JSON.stringify(imageBufferBase64!);
-
-        //console.log("LOG: " + temp );
-
         ws.send(`${Commands.PRINT_SCREEN} ${imageBufferBase64.split(",")[1]}`);
-
-        /* const imageBuffer = Jimp.read(img.image).catch((err) => {
-          console.log(err.message);
-        }); */
-
-        //@ts-ignore
-        /* const imageBufferBase64 = await imageBufferJimp.getBase64Async(
-          Jimp.MIME_PNG
-        ); */
-        //ws.send(`${Commands.PRINT_SCREEN} {${imageBufferBase64}`);
-
         break;
-
-      /* res.writeHead(HTTP_STATUS_CODE_404, CONTENT_TYPE);
-          res.end(JSON.stringify({ message: "Sorry, but this route not found" })); */
     }
   } catch (error) {
-    /* console.error((error as Error).message);
-      res.writeHead(HTTP_STATUS_CODE_500, CONTENT_TYPE);
-      res.end(
-        JSON.stringify({ message: "Something went wrong on th server side" })
-      ); */
+    console.error((error as Error).message);
   }
 };
 
@@ -148,84 +109,6 @@ function parseParams(data: RawData): CommandParams {
     firstParam: arrayParams[1] ? +arrayParams[1] : undefined,
     secondParam: arrayParams[2] ? +arrayParams[2] : undefined,
   };
-}
-
-function drawCircle(radius: number) {
-  robot.mouseToggle("up");
-  const mousePos = robot.getMousePos();
-
-  //robot.moveMouse(x, y);
-  robot.dragMouse(mousePos.x + radius, mousePos.y);
-  robot.mouseToggle("down");
-  for (let i = 0; i <= Math.PI * 2; i += offsetMouseDraw) {
-    const x = mousePos.x + radius * Math.cos(i);
-    const y = mousePos.y + radius * Math.sin(i);
-
-    robot.dragMouse(x, y);
-  }
-}
-
-function drawRectangle(width: number, height: number) {
-  drawLine(width, 0);
-  drawLine(0, height);
-  drawLine(-width, 0);
-  drawLine(0, -height);
-}
-
-function drawLine(offsetX: number, offsetY: number) {
-  const mousePos = robot.getMousePos();
-  if (offsetX !== 0) {
-    const _offsetX = Math.abs(offsetX);
-    for (let i = 0; i <= _offsetX; i += 1) {
-      const x = offsetX > 0 ? mousePos.x + i : mousePos.x - i;
-      const y = mousePos.y;
-
-      robot.dragMouse(x, y);
-    }
-  } else {
-    const _offsetY = Math.abs(offsetY);
-    for (let i = 0; i <= _offsetY; i += 1) {
-      const x = mousePos.x;
-      const y = offsetY > 0 ? mousePos.y + i : mousePos.y - i;
-
-      robot.dragMouse(x, y);
-    }
-  }
-}
-
-function performWithLeftMouseButton(
-  func: Function,
-  param1: number,
-  param2?: number
-) {
-  robot.mouseToggle("down");
-  func(param1, param2);
-  robot.mouseToggle("up");
-}
-
-function screenCaptureToFile(robotBitMap: robot.Bitmap, path: string) {
-  return new Promise((resolve, reject) => {
-    try {
-      const image = new Jimp(robotBitMap.width, robotBitMap.height);
-      let pos = 0;
-      image.scan(
-        0,
-        0,
-        image.bitmap.width,
-        image.bitmap.height,
-        (_, __, idx) => {
-          image.bitmap.data[idx + 2] = robotBitMap.image.readUInt8(pos++);
-          image.bitmap.data[idx + 1] = robotBitMap.image.readUInt8(pos++);
-          image.bitmap.data[idx + 0] = robotBitMap.image.readUInt8(pos++);
-          image.bitmap.data[idx + 3] = robotBitMap.image.readUInt8(pos++);
-        }
-      );
-      image.write(path, resolve);
-    } catch (e) {
-      console.error(e);
-      reject(e);
-    }
-  });
 }
 
 type CommandParams = {
